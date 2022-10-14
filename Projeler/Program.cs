4dd3d0fef7456e4d3290a2ec303c6f238c47ffd1
Fatih Aydın <81859164+FatihAydinn@users.noreply.github.com>
2022-10-14 21:59:43 +0300

    Ortalama Hesaplama

---
+++ Projeler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace csharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int s1 = 0;
+            int s2 = 1;
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            int total = 0;
+            double avarage = 0;
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                total = s1+s2;
+                s1 = s2;
+                s2 = total;
+                array[i] = total;
+            Console.WriteLine(total);
+            }
+            foreach (var item in array)
+            {
+                avarage += item;
+            }
+            System.Console.WriteLine(avarage/n);
+        }
+    }
+}
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
