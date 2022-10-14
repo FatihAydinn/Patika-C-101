964aa551b3ccb8f2b00d66e61c9349cba2cf0fcd
Fatih Aydın <81859164+FatihAydinn@users.noreply.github.com>
2022-10-14 22:13:20 +0300

    Üçgen Çizme

---
+++ Projeler/Üçgen Çizme.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace csharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine("#");
+
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("#");
+                }
+                if (i == n)
+                {
+                    System.Console.Write("#"); // son satır son elemanı eksik kalmaması için
+                }
+            }
+            //Console.ReadKey();
+        }
+    }
+}
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
