98168df70eb3c6f0e70c615c7011aa12f7792f40
Fatih Aydın <81859164+FatihAydinn@users.noreply.github.com>
2022-10-15 20:45:34 +0300

    Algoritma

---
+++ Projeler/Algoritma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+
+namespace csharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+        string algoritma = Console.ReadLine();
+        string[] domma = algoritma.Split(",");
+        int n =  Convert.ToInt32(domma[1]);
+        Console.WriteLine(algoritma.Remove(n,1));
+        Console.ReadKey();
+        }
+    }
+}
