e0c8aa5669a1fbb5c999e10ba1df898f5d3ac367
Fatih Aydın <81859164+FatihAydinn@users.noreply.github.com>
2022-10-15 19:23:31 +0300

    Add files via upload

---
+++ Projeler/Daire Çizme.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace csharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+            for (int l = i; l < n; l++)
+            Console.Write(" ");
+            for (int j = 1; j < i; j++)
+            Console.Write("#");
+            for (int k = 1; k <= i; k++)
+            Console.Write("#");
+            Console.WriteLine(" ");
+            }
+
+            for (int i = n-1; i >= 1; i--)
+            {
+            for (int l = n; l > i; l--)
+            Console.Write(" ");
+            for (int j = 1; j < i; j++)
+            Console.Write("#");
+            for (int k = 1; k <= i; k++)
+            Console.Write("#");
+            Console.WriteLine(" ");
+            }
+        //Console.ReadKey();
+        }
+    }
+}
